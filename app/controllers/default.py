b6ce76dbda4a0ba4d748079755303b5f2a1b58b5
renanzx <renanzx@live.com>
2018-11-14 17:32:16 -0300

    Teste verificação

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,13 @@
-from flask import render_template
+from flask import render_template,request
 from app import app
 
-@app.route("/registro")
+@app.route("/registro/")
 def index():
 	return render_template('registrar.html')
+
+@app.route("/verifica_registro",methods=['GET','POST'])
+def verifica():
+	if(request.method == 'POST'):
+		nome = request.form['nome']
+		sobrenome = request.form['sobrenome']
+
