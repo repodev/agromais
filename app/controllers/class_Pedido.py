7a28750d41ce40dcba15c647d2725c583ec698f6
renanzx <renanzx@live.com>
2018-12-08 14:07:47 -0300

    Encapsulando no poo

--- app/controllers/class_Pedido.py
+++ app/controllers/class_Pedido.py
@@ -1,24 +1,22 @@
 class Pedido():
 	def __init__(self,id_produto,id_comprador,id_produtor,quantidade,valor):
-		self.id_produto = id_produto		
-		self.id_comprador = id_comprador
-		self.id_produtor = id_produtor
-		self.quantidade = quantidade	
-		self.valor = valor
+		self.__id_produto = id_produto		
+		self.__id_comprador = id_comprador
+		self.__id_produtor = id_produtor
+		self.__quantidade = quantidade	
+		self.__valor = valor
 	
-
-
 	def getId_produto(self):
-		return self.id_produto		
+		return self.__id_produto		
 
 	def getId_comprador(self):
-		return self.id_comprador
+		return self.__id_comprador
 
 	def getId_produtor(self):
-		return self.id_produtor
+		return self.__id_produtor
 
 	def getQuantidade(self):
-		return self.quantidade
+		return self.__quantidade
 
 	def getValor(self):
-		return self.valor
+		return self.__valor
--- app/controllers/class_PerfilProdutor.py
+++ app/controllers/class_PerfilProdutor.py
@@ -4,27 +4,27 @@ class PerfilProdutor(PerfilComprador):
 
 	def __init__(self,nome,sobrenome,contato,cidade,bairro,endereco,cpf,email,senha,nome_loja,cnpj,contato_comercial,endereco_comercial,descricao_loja,foto_loja):
 		super().__init__(nome,sobrenome,contato,cidade,bairro,endereco,cpf,email,senha)
-		self.nome_loja = nome_loja
-		self.cnpj = cnpj
-		self.contato_comercial = contato_comercial
-		self.endereco_comercial = endereco_comercial
-		self.descricao_loja = descricao_loja
-		self.foto_loja = foto_loja
+		self.__nome_loja = nome_loja
+		self.__cnpj = cnpj
+		self.__contato_comercial = contato_comercial
+		self.__endereco_comercial = endereco_comercial
+		self.__descricao_loja = descricao_loja
+		self.__foto_loja = foto_loja
 
 	def getNome_loja(self):
-		return self.nome_loja
+		return self.__nome_loja
 
 	def getCnpj(self):
-		return self.cnpj
+		return self.__cnpj
 
 	def getDescricao_loja(self):
-		return self.descricao_loja
+		return self.__descricao_loja
 
 	def getContato_comercial(self):
-		return self.contato_comercial
+		return self.__contato_comercial
 
 	def getEndereco_comercial(self):
-		return self.endereco_comercial
+		return self.__endereco_comercial
 
 	def getFoto(self):
-		return self.foto_loja
+		return self.__foto_loja
